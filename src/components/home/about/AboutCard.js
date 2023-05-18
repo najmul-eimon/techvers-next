0368220f4f9d29656e1f7ddb41209f9b694e6535
najmul-eimon <najmul.eimon@intelli.global>
2023-05-18 12:36:15 +0600

    about image update

--- src/components/home/about/AboutCard.js
+++ src/components/home/about/AboutCard.js
@@ -1,10 +1,12 @@
+import Image from "next/image";
+
 const AboutCard = ({data}) => {
   const {icon, count, title, description} = data;
 
   return (
     <div className="sm:col-span-2 lg:col-span-1 rounded-xl shadow-[0px_0px_21px_rgba(92,92,92,0.12)] p-6 bg-white">
       <div className="flex items-center gap-3">
-        <img src={icon} alt="Star"/>
+        <Image src={icon} alt="Icon" width={27} height={28}/>
         <h3 className="text-primary-text text-2xl md:text-[32px] font-bold">{count}+</h3>
       </div>
       <h4 className="text-primary-text font-inter text-sm md:text-base font-bold pt-4 pb-5 md:py-5">{title}</h4>
