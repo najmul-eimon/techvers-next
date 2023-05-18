00c3c53250665ddba80f132a6036c0de1d0f377e
najmul-eimon <najmul.eimon@intelli.global>
2023-05-18 12:56:28 +0600

    font update

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -31,12 +31,6 @@ const Layout = ({children}) => {
 
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet"/>
-      </Head>
       <SharedContext.Provider value={allContexts}>
         <Header/>
         <main>
