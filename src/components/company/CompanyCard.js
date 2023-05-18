a106e1a221c5cb88e64f7d886641ee26cb884364
najmul-eimon <najmul.eimon@intelli.global>
2023-05-18 12:50:00 +0600

    company card update

--- src/components/company/CompanyCard.js
+++ src/components/company/CompanyCard.js
@@ -1,6 +1,4 @@
 import Image from 'next/image';
-import CountUp from 'react-countup';
-import VisibilitySensor from 'react-visibility-sensor';
 
 const CompanyCard = ({data}) => {
   const {id, title, count, icon} = data;
@@ -9,13 +7,7 @@ const CompanyCard = ({data}) => {
     <div className="w-1/2 md:w-1/4 text-center">
       <Image src={icon} className="mx-auto" alt="trophy" width={49} height={48}/>
       <h3 className="py-3 md:py-6 text-white text-[32px] lg:text-[56px] font-bold">
-        <CountUp start={0} end={count}>
-          {({ countUpRef, start }) => (
-            <VisibilitySensor onChange={start}>
-              <span className="counter" ref={countUpRef}/>
-            </VisibilitySensor>
-          )}
-        </CountUp>
+        {count}
         {id === 'c2' && '+'}
       </h3>
       <p className="font-inter text-sm md:text-base text-white font-medium">{title}</p>
